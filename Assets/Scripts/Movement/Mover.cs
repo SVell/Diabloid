9ad74208017b90149649edd7a3673809028589c9
Viacheslav <surzhenkoviacheslav@gmail.com>
2020-06-19 11:42:19 +0300

    Initial commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Animator anim;
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+
+        Animate();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            navMeshAgent.destination = hit.point;
+        }
+    }
+
+    private void Animate()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        anim.SetFloat("ForwardSpeed", speed);
+
+    }
+}
