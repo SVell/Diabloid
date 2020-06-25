f9bb5f77b5597311d49d5e9d32e04463d2e7bfdc
Viacheslav <surzhenkoviacheslav@gmail.com>
2020-06-25 11:12:06 +0300

    Loader

---
+++ Assets/Scripts/SceneManagment/Fader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        private CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(2f);
+            yield return FadeIn(1f);
+        }
+        
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagment/Portal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = System.Diagnostics.Debug;
+using UnityEngine.Diagnostics;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+            A,B,C,D,E
+        }
+        
+        [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private DestinationIdentifier destination;
+        [SerializeField] private float fadeOutTime = 1f;
+        [SerializeField] private float fadeInTime = 1f;
+        [SerializeField] private float fadeWaitTime = 0.5f;
+        private void Start()
+        {
+            
+        }
+
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Transition());
+            }
+        }
+        
+        private IEnumerator Transition()
+        {
+           
+            if (sceneToLoad < 0)
+            {
+                yield break;
+            }
+
+            Fader fader = FindObjectOfType<Fader>();
+            
+            DontDestroyOnLoad(gameObject);
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if(portal.destination != destination) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+    }
+}
